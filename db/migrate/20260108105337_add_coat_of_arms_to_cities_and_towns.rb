b60c9a39b9d861c2a68b5c1d7e23e3f79413a019
Maris Locmelis <neznajki@gmail.com>
2026-01-08 13:11:36 +0200

    put valid coast of arms links

--- db/migrate/20260108105337_add_coat_of_arms_to_cities_and_towns.rb
+++ db/migrate/20260108105337_add_coat_of_arms_to_cities_and_towns.rb
@@ -3,16 +3,46 @@ class AddCoatOfArmsToCitiesAndTowns < ActiveRecord::Migration[7.1]
     add_column :cities_and_towns, :coat_of_arms, :string
 
     data = [
-      {"city" => "Daugavpils", "coat_of_arms" => "https://upload.wikimedia.org/wikipedia/commons/5/51/Coat_of_arms_of_Daugavpils.svg"},
-      {"city" => "Jelgava", "coat_of_arms" => "https://commons.wikimedia.org/wiki/Category:Coats_of_arms_of_Jelgava"},
-      {"city" => "Jurmala", "coat_of_arms" => "https://upload.wikimedia.org/wikipedia/commons/1/1b/Coat_of_Arms_of_Jūrmala.svg"},
-      {"city" => "Liepaja", "coat_of_arms" => "https://upload.wikimedia.org/wikipedia/commons/1/11/Coat_of_Arms_of_Liepāja.svg"},
-      {"city" => "Rezekne", "coat_of_arms" => "https://commons.wikimedia.org/wiki/Category:Coats_of_arms_of_Rēzekne"},
-      {"city" => "Riga", "coat_of_arms" => "https://upload.wikimedia.org/wikipedia/commons/8/84/Coat_of_Arms_of_Riga.svg"},
-      {"city" => "Ventspils", "coat_of_arms" => "https://upload.wikimedia.org/wikipedia/commons/5/5f/Coat_of_Arms_of_Ventspils.svg"},
-      {"city" => "Sigulda", "coat_of_arms" => "https://commons.wikimedia.org/wiki/Category:Coats_of_arms_of_Sigulda"},
-      {"city" => "Cesis", "coat_of_arms" => "https://upload.wikimedia.org/wikipedia/commons/4/4e/C%C4%93sis_COA.svg"},
-      {"city" => "Kuldiga", "coat_of_arms" => "https://upload.wikimedia.org/wikipedia/commons/2/24/Coat_of_Arms_of_Kuld%C4%ABga.svg"}
+      {
+        "city": "Daugavpils",
+        "coat_of_arms": "https://upload.wikimedia.org/wikipedia/commons/a/a2/Coat_of_arms_of_Daugavpils.svg"
+      },
+      {
+        "city": "Jelgava",
+        "coat_of_arms": "https://upload.wikimedia.org/wikipedia/commons/e/e4/Jelgava_COA_small.svg"
+      },
+      {
+        "city": "Jurmala",
+        "coat_of_arms": "https://upload.wikimedia.org/wikipedia/commons/e/ea/Coat_of_Arms_of_J%C5%ABrmala.svg"
+      },
+      {
+        "city": "Liepaja",
+        "coat_of_arms": "https://upload.wikimedia.org/wikipedia/commons/0/03/Coat_of_Arms_of_Liep%C4%81ja.svg"
+      },
+      {
+        "city": "Rezekne",
+        "coat_of_arms": "https://upload.wikimedia.org/wikipedia/commons/5/5a/Coat_of_Arms_of_R%C4%93zekne.svg"
+      },
+      {
+        "city": "Riga",
+        "coat_of_arms": "https://upload.wikimedia.org/wikipedia/commons/9/99/Greater_Coat_of_Arms_of_Riga_-_for_display.svg"
+      },
+      {
+        "city": "Ventspils",
+        "coat_of_arms": "https://upload.wikimedia.org/wikipedia/commons/f/f3/Coat_of_Arms_of_Ventspils.svg"
+      },
+      {
+        "city": "Sigulda",
+        "coat_of_arms": "https://upload.wikimedia.org/wikipedia/commons/f/f6/Coat_of_Arms_of_Sigulda.svg"
+      },
+      {
+        "city": "Cesis",
+        "coat_of_arms": "https://upload.wikimedia.org/wikipedia/commons/9/94/Cesis_COA.svg"
+      },
+      {
+        "city": "Kuldiga",
+        "coat_of_arms": "https://upload.wikimedia.org/wikipedia/commons/4/42/Kuld%C4%ABgas_novads_COA.svg"
+      }
     ]
 
     data.each do |item|
